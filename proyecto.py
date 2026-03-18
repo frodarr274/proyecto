1b0f865c5a1b90d70adc69f375507743b71833e1
frodarr274 <frodarr274@g.educaand.es>
2026-03-18 10:23:24 +0100

    vamoo

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,1 @@
-conexion sqlite3.connect("proyecto.db")
-cursor=conexion.cursor()
-cursor execute('''
-INSERT INTO España(provincia,comida,id_identificación) VALUES (sevilla, pescaito,1)''')
+import sqlite
