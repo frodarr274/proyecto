d5cbcae3d46023e073118b52c3f416bebdb9a767
Jesusito22 <jcrugal849@g.educaand.es>
2026-03-18 11:12:52 +0100

    nueva las comunidade

--- proyecto.py
+++ proyecto.py
@@ -3,14 +3,14 @@ import sqlite3
 conexion = sqlite3.connect("vivaespana.db")
 cursor = conexion.cursor()
 
-cursor.execute(''' INSERT INTO comunidades (id_comunidades,nombre,poblacion) VALUES (?, ?, ?)''', (1,"Andalucía",8733535))
+cursor.execute(''' INSERT INTO comunidades (id_comunidad,nombre,poblacion) VALUES (?, ?, ?)''', (1,"Andalucía",8733535))
 
 comunidades= [
     (2,"Madrid",3506730),
     (3,"Cataluña",8012231),
     (4,"Murcia",479405)
 ]
-cursor.executemany(''' INSERT INTO comunidades (id_comunidades,nombre,poblacion) VALUES(?, ?, ?)''', comunidades)
+cursor.executemany(''' INSERT INTO comunidades (id_comunidad,nombre,poblacion) VALUES(?, ?, ?)''', comunidades)
 
-conexion.comit()
+conexion.commit()
 conexion.close()
