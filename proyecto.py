1b3b4c08aab61329675238c493a02de5a8d61480
frodarr274 <frodarr274@g.educaand.es>
2026-04-10 11:10:22 +0200

    ojo

--- proyecto.py
+++ proyecto.py
@@ -1,14 +1,20 @@
 import sqlite3
+#MENÚ
 
-print("----MENÚ----")
-print("1.Crear")
-print("2.Leer")
-print("3.Actualizar")
-print("4.Borrar")
-print("--------------------------------")
-int(input("Escribe lo que quieres hacer:"))
-
-
+while True:
+    print("----MENÚ----")
+    print("1.Crear")
+    print("2.Leer")
+    print("3.Actualizar")
+    print("4.Borrar")
+    print("--------------------------------")
+    int(input("Escribe lo que quieres hacer:"))
+    continuar=input("¿Quieres hacer algo más?")
+      
+    if continuar=="no":
+        print("Adiós")
+        break
+    while menu !="no" 
 
                                                                                             
 
