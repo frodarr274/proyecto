b909f0d9b8c879329b34ab68471694ff7bbb8610
Jesusito22 <jcrugal849@g.educaand.es>
2026-04-22 10:31:53 +0200

    boceto del proyecto

--- proyecto.py
+++ proyecto.py
@@ -1,7 +1,5 @@
 import sqlite3
-#MENÚ
-
-while True:
+def menu ():
     print("----MENÚ----")
     print("1.Crear")
     print("2.Leer")
@@ -10,13 +8,43 @@ while True:
     print("--------------------------------")
     int(input("Escribe lo que quieres hacer:"))
     continuar=input("¿Quieres hacer algo más?")
-      
-    if continuar=="no":
-        print("Adiós")
-        break
-    while menu !="no" 
 
+    if continuar=="no":
+        print ("Adios")
+        break
+    while menu !="no"
+
+conexion = sqlite3.connect("proyecto.db")
+cursor=conexion.cursor()
+try:
+    cursor.execute(''' INSERT INTO comunidad (nombre, provincia, poblacion) VALUES (?,?,?)''')
+
+    comunidad = [
+
+
+
+    ]
+
+    cursor.executemany(''' INSERT INTO comunidad (nombre, provincia, poblacion) VALUES (?,?,?)''' ,provincia)
+
+    cursor.execute('''SELECT * FROM comunidad''')
+   resultados= cursor.fetchall()
+
+    for fila in resultados:
+    print (fila)
+
+
+    cursor.execute('''UPDATE comunidad SET poblacion = ? WHERE nombre = ? ''')
                                                                                             
+
+    cursor.execute(''' DELETE FROM comunidad WHERE nombre = ?''')
+
+    conexion.commit()
+except Excepcion as e:
+    print ("Ha ocurrido un error con los datos selecionados", e)
+    conexion.rollback()
+finally:
+    conexion.close()                                                                                        
 
 
 
