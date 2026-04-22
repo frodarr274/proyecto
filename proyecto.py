4bc90d7dfa1e279103a9b701e7798a9275fb245d
Jesusito22 <jcrugal849@g.educaand.es>
2026-04-22 11:11:35 +0200

    insertando datos en el boceto

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,4 @@
+#Menú del sistema 
 import sqlite3
 def menu ():
     print("----MENÚ----")
@@ -14,34 +15,54 @@ def menu ():
         break
     while menu !="no"
 
+#Para insertar datos
 conexion = sqlite3.connect("proyecto.db")
 cursor=conexion.cursor()
 try:
-    cursor.execute(''' INSERT INTO comunidad (nombre, provincia, poblacion) VALUES (?,?,?)''')
+    cursor.execute(''' INSERT INTO comunidades (id_comunidad,nombre,poblacion) VALUES (?,?,?)''' (1,"Andalucía",125000))
 
-    comunidad = [
+    comunidades = [
+        (2,"Madrid", 20000),
+        (3,"Extremadura",45000),
+        (4,"Galicia", 90000)
+    ]
 
+    cursor.executemany(''' INSERT INTO comunidades (id_comunidad,nombre,poblacion) VALUES (?,?,?)''' ,comunidades)
+
+    cursor.execute(''' INSERT INTO provincias (id_provincia,nombre,id_comunidad VALUES (?,?,?)''' (1,"Sevilla",1))
+
+    provincias=[
 
 
     ]
 
-    cursor.executemany(''' INSERT INTO comunidad (nombre, provincia, poblacion) VALUES (?,?,?)''' ,provincia)
+    cursor.executemany(''' INSERT INTO provincias (id_provincia,nombre,id_comunidad) VALUES (?,?,?)''' ,provincias)
 
-    cursor.execute('''SELECT * FROM comunidad''')
+    cursor.execute(''' INSERT INTO ciudad (id_ciudad,nombre,id_provincia,alcalde) VALUES (?,?,?)''')
+
+    ciudad= [
+
+
+    ]
+    cursor.executemany(''' INSERT INTO ciudad (id_ciudad,nombre,id_provincia,alcalde) VALUES (?,?,?)''' ,ciudad)
+    #Para selecionar datos 
+    cursor.execute('''SELECT * FROM comunidades''')
    resultados= cursor.fetchall()
 
     for fila in resultados:
     print (fila)
 
+    #Para borrar o actualizar datos
 
-    cursor.execute('''UPDATE comunidad SET poblacion = ? WHERE nombre = ? ''')
+    cursor.execute('''UPDATE comunidades SET poblacion = ? WHERE nombre = ? ''')
                                                                                             
 
-    cursor.execute(''' DELETE FROM comunidad WHERE nombre = ?''')
+    cursor.execute(''' DELETE FROM comunidades WHERE nombre = ?''')
 
     conexion.commit()
+    #Para realizar transacciones (se requeria de esto una vez que los datos esten ordenados y los comandos funcionen perfectamente)
 except Excepcion as e:
-    print ("Ha ocurrido un error con los datos selecionados", e)
+    print ("Ha ocurrido un error con los datos que usted ha seleccionado", e)
     conexion.rollback()
 finally:
     conexion.close()                                                                                        
